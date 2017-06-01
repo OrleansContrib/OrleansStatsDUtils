545b55e26e85db4c7ce29a7d0f07bd460ab4b5c2
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2017-06-01 16:01:05 +0300

    Update StatsDStatisticsProvider

--- src/StatsDStatisticsProvider.cs
+++ src/StatsDStatisticsProvider.cs
@@ -11,64 +11,42 @@ using StatsdClient;
 
 namespace SBTech.OrleansStatsDUtils
 {
-    
 
-    public class StatsDStatisticsProvider : IConfigurableSiloMetricsDataPublisher,
+
+    public class StatsDStatisticsProvider : StatsdProvider, 
+                                            IConfigurableSiloMetricsDataPublisher,
                                             IStatisticsPublisher,
                                             IProvider
-    {   
-        State _state = new State();
-        Logger _logger;
-
-        /// <summary>
-        /// Name of the provider
-        /// </summary>
-        public string Name { get; private set; }
-
-        /// <summary>
-        /// Closes provider
-        /// </summary>        
-        public Task Close() => TaskDone.Done;
-
-        /// <summary>
-        /// Initialization of StatsDStatisticsProvider
-        /// </summary>        
-        public Task Init(string name, IProviderRuntime providerRuntime, IProviderConfiguration config)
+    {
+        public StatsdStatisticsProvider()
         {
-            Name = name;
-            _state.Id = providerRuntime.SiloIdentity;
-            _logger = providerRuntime.GetLogger(typeof(StatsDStatisticsProvider).Name);
+            StatsdConfiguration.CheckConfiguration();
+        }
 
-            if (config.Properties.ContainsKey("StatsDServerName"))
-                _state.StatsDServerName = config.Properties["StatsDServerName"];
+        public Task Init(string deploymentId, string storageConnectionString, SiloAddress siloAddress, string siloName, IPEndPoint gateway, string hostName)
+        {
+            _state.DeploymentId = deploymentId;
+            _state.SiloName = siloName;
+            _state.GatewayAddress = gateway.ToString();
+            _state.HostName = hostName;
 
-            if (config.Properties.ContainsKey("StatsDServerPort"))
-                _state.StatsDServerPort = int.Parse(config.Properties["StatsDServerPort"]);
-            
-            if (config.Properties.ContainsKey("StatsDPrefix"))
-                _state.StatsDPrefix = config.Properties["StatsDPrefix"];
+            return TaskDone.Done;
+        }
 
-            if (config.Properties.ContainsKey("StatsDMaxUdpPacketSize"))
-                _state.StatsDMaxUdpPacketSize = int.Parse(config.Properties["StatsDMaxUdpPacketSize"]);
-
-            var metricsConfig = new MetricsConfig
-            {
-                StatsdServerName = _state.StatsDServerName,
-                StatsdServerPort = _state.StatsDServerPort,
-                Prefix = string.IsNullOrEmpty(_state.StatsDPrefix)
-                            ? $"{_state.HostName.ToLower()}.{_state.SiloName.ToLower()}"
-                            : $"{_state.StatsDPrefix.ToLower()}.{_state.HostName.ToLower()}",
-                StatsdMaxUDPPacketSize = _state.StatsDMaxUdpPacketSize
-            };
-
-            Metrics.Configure(metricsConfig);
+        public Task Init(bool isSilo, string storageConnectionString, string deploymentId, string address, string siloName, string hostName)
+        {
+            _state.DeploymentId = deploymentId;
+            _state.IsSilo = isSilo;
+            _state.SiloName = siloName;
+            _state.Address = address;
+            _state.HostName = hostName;
 
             return TaskDone.Done;
         }
 
         /// <summary>
         /// Initialization of configuration for Silo
-        /// </summary>        
+        /// </summary> 
         public void AddConfiguration(string deploymentId, bool isSilo, string siloName, SiloAddress address, IPEndPoint gateway, string hostName)
         {
             _state.DeploymentId = deploymentId;
@@ -85,7 +63,7 @@ namespace SBTech.OrleansStatsDUtils
         public Task ReportMetrics(ISiloPerformanceMetrics metricsData)
         {
             if (_logger != null && _logger.IsVerbose3)
-                _logger.Verbose3("StatsDStatisticsProvider.ReportMetrics called with metrics: {0}, name: {1}, id: {2}.", metricsData, _state.SiloName, _state.Id);
+                _logger.Verbose3($"{ nameof(StatsdStatisticsProvider)}.ReportMetrics called with metrics: {0}, name: {1}, id: {2}.", metricsData, _state.SiloName, _state.Id);
 
             try
             {
@@ -95,10 +73,10 @@ namespace SBTech.OrleansStatsDUtils
             catch (Exception ex)
             {
                 if (_logger != null && _logger.IsVerbose)
-                    _logger.Verbose("StatsDStatisticsProvider.ReportMetrics failed: {0}", ex);
+                    _logger.Verbose($"{ nameof(StatsdStatisticsProvider)}.ReportMetrics failed: {0}", ex);
 
                 throw;
-            }
+            }            
         }
 
         /// <summary>
@@ -107,11 +85,13 @@ namespace SBTech.OrleansStatsDUtils
         public Task ReportStats(List<ICounter> statsCounters)
         {
             if (_logger != null && _logger.IsVerbose3)
-                _logger.Verbose3("StatsDStatisticsProvider.ReportStats called with {0} counters, name: {1}, id: {2}", statsCounters.Count, _state.SiloName, _state.Id);
+                _logger.Verbose3($"{ nameof(StatsdStatisticsProvider)}.ReportStats called with {0} counters, name: {1}, id: {2}", statsCounters.Count, _state.SiloName, _state.Id);
 
             try
             {
-                foreach (var counter in statsCounters.Where(cs => cs.Storage == CounterStorage.LogAndTable))
+                var counters = statsCounters.Where(cs => cs.Storage == CounterStorage.LogAndTable);
+
+                foreach (var counter in counters)
                 {
                     SendStats(counter);
                 }
@@ -120,38 +100,27 @@ namespace SBTech.OrleansStatsDUtils
             catch (Exception ex)
             {
                 if (_logger != null && _logger.IsVerbose)
-                    _logger.Verbose("StatsDStatisticsProvider.ReportStats failed: {0}", ex);
+                    _logger.Verbose($"{ nameof(StatsdStatisticsProvider)}.ReportStats failed: {0}", ex);
 
                 throw;
-            }
+            }            
         }
 
-        public Task Init(string deploymentId, string storageConnectionString, SiloAddress siloAddress, string siloName, IPEndPoint gateway, string hostName) => TaskDone.Done;
-
-        public Task Init(bool isSilo, string storageConnectionString, string deploymentId, string address, string siloName, string hostName) => TaskDone.Done;
-
-        static void SendSiloMetrics(ISiloPerformanceMetrics metricsData)
+        private static void SendSiloMetrics(ISiloPerformanceMetrics metricsData)
         {
-            Metrics.GaugeAbsoluteValue("cpu_usage", metricsData.CpuUsage);
-            Metrics.GaugeAbsoluteValue("total_physical_memory", metricsData.TotalPhysicalMemory);
-            Metrics.GaugeAbsoluteValue("available_physical_memory", metricsData.AvailablePhysicalMemory);
-            Metrics.GaugeAbsoluteValue("memory_usage", metricsData.MemoryUsage);            
-            Metrics.GaugeAbsoluteValue("total_physical_memory", metricsData.TotalPhysicalMemory);
-            Metrics.GaugeAbsoluteValue("send_queue_length", metricsData.SendQueueLength);
-            Metrics.GaugeAbsoluteValue("receive_queue_length", metricsData.ReceiveQueueLength);
-            Metrics.GaugeAbsoluteValue("sent_messages", metricsData.SentMessages);
-            Metrics.GaugeAbsoluteValue("received_messages", metricsData.ReceivedMessages);
             Metrics.GaugeAbsoluteValue("activations_count", metricsData.ActivationCount);
             Metrics.GaugeAbsoluteValue("recently_used_activations", metricsData.RecentlyUsedActivationCount);
             Metrics.GaugeAbsoluteValue("request_queue_length", metricsData.RequestQueueLength);
             Metrics.GaugeAbsoluteValue("is_overloaded", metricsData.IsOverloaded ? 1 : 0);
             Metrics.GaugeAbsoluteValue("client_count", metricsData.ClientCount);
+
+            SendCoreMetrics(metricsData);
         }
 
-        static void SendStats(ICounter counter)
+        private static void SendStats(ICounter counter)
         {
-            var valueStr = counter.IsValueDelta 
-                ? counter.GetDeltaString() 
+            var valueStr = counter.IsValueDelta
+                ? counter.GetDeltaString()
                 : counter.GetValueString();
 
             float value;
